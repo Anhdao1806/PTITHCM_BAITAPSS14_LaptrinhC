6e9d3510ec7ee4b062dc9bc0ffe76968c91fa50b
Anhdao1806 <daoleho123@gmail.com>
2025-01-02 09:59:45 +0700

    Add files via upload

---
+++ baitap2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char chuoi[] = "123456789";
+    for (int i = 0; i < strlen(chuoi); i++) {
+    printf("%c ", chuoi[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
